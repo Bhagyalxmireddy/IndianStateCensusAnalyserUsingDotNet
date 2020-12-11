fe66a4d7fedce193db76f8fe4c68f573aead4b17
Bhagi <puppyreddy1997@gmail.com>
2020-12-11 21:56:17 +0530

    [Bhagyalaxmi]ADDED:Added code to LoadCensusData to csvFile and added the Testcases for the csvFile

---
+++ IndiaStateCensusAnalyser/CensusAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using IndiaStateCensusAnalyser.POCO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndiaStateCensusAnalyser
+{
+    public class CensusAnalyser 
+    {
+        public enum Country
+        {
+            INDIA,
+            US
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+}
